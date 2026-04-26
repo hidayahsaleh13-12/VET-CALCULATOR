e37fa5fa27c9fa3dfd6f73ea2fe9885929340a75
hidayahsaleh13-12 <hidayahsaleh13@gmail.com>
2026-04-26 12:38:24 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('install', (e) => {
+  console.log('Service Worker Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
+});
